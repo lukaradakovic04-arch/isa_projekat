76689d5dfb84d8d612c66de11c9376087c64059c
Chuurosangue <luka.radakovic.04@gmail.com>
2026-08-24 16:11:51 +0200

    Add files via upload

---
+++ database/create_database.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE IF NOT EXISTS isa_library
+    CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
+
+USE isa_library;
+
+-- Tabele automatski kreira Hibernate zato sto je u application.properties:
+-- spring.jpa.hibernate.ddl-auto=update
